bbee98f2bed8d0032a9867d3ebfd341705b24be8
Jessica <duellje@oregonstate.edu>
2022-01-10 21:45:20 -0500

    Added second tab

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Jessica  Duell on 1/9/22.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("ConversionViewController loaded its view")
+    }
+  
+}
+
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by Jessica  Duell on 1/10/22.
+//
+
+import UIKit
+
+class MapViewController: UIViewController{
+     override func viewDidLoad() {
+        super.viewDidLoad()
+        print("MapViewController loaded its view")
+    }
+}
